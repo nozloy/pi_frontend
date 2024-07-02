ef01c2a5c1ce89dcd6a2d732f50bbced5b2c8edb
NoZloy <nozloy@gmail.com>
2024-07-02 14:01:51 +0300

    update: src/app/download/page.tsx

---
+++ src/app/download/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+	return (
+		<div className='flex min-h-screen flex-col items-center justify-between p-20 bg-slate-200'>
+			DOWNLOAD
+		</div>
+	)
+}
