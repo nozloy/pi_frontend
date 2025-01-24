6e7ab97229d22ca5cb481bd7fedb0585ce71c4d3
NoZloy <nozloy@gmail.com>
2025-01-24 05:55:12 +0300

    fix

--- src/app/api/relay/route.ts
+++ src/app/api/relay/route.ts
@@ -4,16 +4,17 @@ export async function POST(req: NextRequest) {
 	const { port, action } = await req.json()
 
 	if (action === 'on') {
-		await fetch(`http://localhost:5000/relay/on`, {
+		await fetch(`http://127.0.0.1:5000/relay/on`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({ port }),
 		})
+		console.log(`Relay ${port} turned on`)
 		return NextResponse.json({ message: `Relay ${port} turned on` })
 	} else if (action === 'off') {
-		await fetch(`http://localhost:5000/relay/off`, {
+		await fetch(`http://127.0.0.1:5000/relay/off`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -22,13 +23,15 @@ export async function POST(req: NextRequest) {
 		})
 		return NextResponse.json({ message: `Relay ${port} turned off` })
 	} else if (action === 'temporary_on') {
-		await fetch(`http://localhost:5000/relay/temporary_on`, {
+		await fetch(`http://127.0.0.1:5000/relay/temporary_on`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({ port }),
 		})
+		console.log(`Relay ${port} turned on`)
+
 		return NextResponse.json({
 			message: `Relay ${port} turned on for 15 seconds`,
 		})
